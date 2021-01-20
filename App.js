574419dde59929d9be11fea64ef385f6a2f5090c
patirosa95 <patirosa995@gmail.com>
2021-01-20 10:53:59 -0300

    create text counter

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
+import TextCounter from './TextCounter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextCounter/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight + 5,
+    paddingLeft: 15,
+    paddingRight: 15
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
---
+++ TextCounter.js
@@ -0,0 +1,25 @@
+import React, {Component, COmponent} from 'react';
+import { View, Text, TextInput} from 'react-native';
+
+class TextCounter extends Component{
+    state = {
+        text: ''
+    }
+
+    render(){
+        const { state } = this;
+        return(
+            <View>
+                <TextInput
+                    multiline={true}
+                    numberOfLines={5}
+                    value={ state.text }
+                    style={{borderColor:'gray', borderWidth:1}}
+                />
+                <Text>Total: { state.text.length }/100</Text>
+            </View>
+        );
+    }
+}
+
+export default TextCounter;
